e7d58dc1fbaa393250b86add5910e8bdce0060b2
akdprince <anik@Cuseng01.palonet.org>
2026-08-02 23:10:24 +0600

    none return

--- retunFunc.py
+++ retunFunc.py
@@ -1,8 +1,14 @@
-def calculate_mb(gb_value):
-    result = gb_value * 1024
-    return result  # This sends the number back to the program
+# def calculate_mb(gb_value):
+#     result = gb_value * 1024
+#     return result  # This sends the number back to the program
 
-# You can now save that returned answer into a new variable
-my_ram_mb = calculate_mb(16)
+# # You can now save that returned answer into a new variable
+# my_ram_mb = calculate_mb(16)
 
-print(f"I have {my_ram_mb} MB of RAM.")  # Output: I have 16384 MB of RAM.
+# print(f"I have {my_ram_mb} MB of RAM.")  # Output: I have 16384 MB of RAM.
+
+def say_hello():
+    print("Hello!")
+
+result = say_hello()
+print(result)  # Output: None (because nothing was explicitly returned)
